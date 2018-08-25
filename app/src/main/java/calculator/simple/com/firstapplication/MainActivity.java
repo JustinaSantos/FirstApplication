277060e05ae9ed4112b872e3d193aa555a7acdef
Justina Santos <clangriz1@gmail.com>
2018-08-25 14:56:43 +0800

    Initial commit

---
+++ app/src/main/java/calculator/simple/com/firstapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package calculator.simple.com.firstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+    }
+}
